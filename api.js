4efe7db0b140262e500b6bf66df7877677a09d6c
Tejasj77 <tejasjoshitj@gmail.com>
2020-08-30 22:08:14 +0530

    Second Staging

--- api.js
+++ api.js
@@ -1,1 +1,7 @@
-console.log("We are here")
+const bt = document.querySelector('naav');
+document.addEventListener('click',function(){
+    console.log("We are here");
+    let insta = fetch('Installs.json');
+    console.log(insta);
+
+})
